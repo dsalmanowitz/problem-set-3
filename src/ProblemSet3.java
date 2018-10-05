e1e167d15c25819a35fa40bcc27ff0192d2ece5c
dsalmanowitz <43217565+dsalmanowitz@users.noreply.github.com>
2018-10-05 13:45:39 -0400

    Update ProblemSet3.java

--- src/ProblemSet3.java
+++ src/ProblemSet3.java
@@ -217,7 +217,7 @@ public class ProblemSet3 {
 		if (a == 13) {
 			System.out.println("0");
 		} else if (b == 13) {
-			System.out.print(a);
+			System.out.println(a);
 		} else if (c == 13) {
 			System.out.println(a+b);
 		} else {
